7383f01995522a7b636c45e32e8d519458b673dc
Malavika C Biju <malavikacbiju625@gmail.com>
2025-09-18 15:52:31 +0530

    add

---
+++ dic.py
@@ -0,0 +1,5 @@
+#dict has key value pair,key must be unique and immutable, and mutable,created using {} or dict(), 
+#accessed using key
+student={"name":"John","age":21,"mark1":85,"mark2":90}
+print(student)
+     
--- set.py
+++ set.py
@@ -1,1 +1,11 @@
 #set syntax ,set can create only by set(),it is unordered collection of unique elements,unindeexing ,
+a=set()
+n=int(input("Enter the number of elements:"))
+for i in range(0,n):
+    no=int(input())
+    a.add(no)#add() method is used to add elements in set
+print(a)
+s=0
+for x in a:
+        s+=x
+print(f"The sum is {s}")
